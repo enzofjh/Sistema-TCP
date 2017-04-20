8b6218599d1cda7008c3639c9d3ab2121b334018
Enzo Jimenez <enzofjh@gmail.com>
2017-04-20 09:56:33 -0600

    First push

---
+++ Cliente/Paquetes.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace Cliente
+{
+    public class Paquetes
+    {
+        public static Paquete InicioSesion(string email, string password)
+        {
+            var package = new List<string> { email, password };
+            return new Paquete("login", Mapa.Serializar(package));
+        }
+    }
+}
---
+++ Servidor/Mapa.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Servidor
+{
+    public static class Mapa
+    {
+        public static string Serializar(List<string> lista)
+        {
+            if (lista.Count == 0)
+            {
+                return "";
+            }
+
+            bool esElPrimero = true;
+            var salida = new StringBuilder();
+
+            foreach (var linea in lista)
+            {
+                if (esElPrimero)
+                {
+                    salida.Append(linea);
+                    esElPrimero = false;
+                }
+                else
+                {
+                    salida.Append(string.Format(",{0}",linea));
+                }
+            }
+            return salida.ToString();
+        }
+
+        public static List<string> Deserializar(this string entrada)
+        {
+            string str = entrada;
+            var list = new List<string>();
+
+            if (string.IsNullOrEmpty(str))
+            {
+                return list;
+            }
+
+            try
+            {
+                foreach (string linea in entrada.Split(','))
+                {
+                    list.Add(linea);
+                }
+            }
+            catch
+            {
+                return null;
+            }
+            return list;
+        }
+    }
+}
